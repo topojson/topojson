58df72c98c06abe3b2e854fff8b609e161bd31ee
Mike Bostock <mbostock@gmail.com>
2012-11-16 02:32:54 -0800

    Rounding.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var pointStore = require("./lib/topojson/point-store");
+var pointStore = require("./lib/topojson/point-store"),
+    type = require("./lib/topojson/type");
 
 exports.toTopo = function(geoJson) {
-  var topo = {
-    type: "Topology",
-    objects: [],
-    coordinates: []
-  };
-
   return pointStore(Object.keys(geoJson).map(function(k) { return geoJson[k]; }));
 };
 
 exports.toGeo = function(topoJson) {
   return topoJson;
+};
+
+exports.round = function(geoJson, precision) {
+  precision = Math.pow(10, precision);
+  type({
+    point: function(point) {
+      point[0] = Math.round(point[0] * precision) / precision;
+      point[1] = Math.round(point[1] * precision) / precision;
+    }
+  }).object(geoJson);
+  return geoJson;
 };
