73e061789fd3c5be4d7f248d9ab6f0eb199cf768
Mike Bostock <mbostock@gmail.com>
2012-11-15 19:18:33 -0800

    Unique point determination.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+var pointStore = require("./lib/topojson/point-store");
+
 exports.toTopo = function(geoJson) {
-  return geoJson;
+  var topo = {
+    type: "Topology",
+    objects: [],
+    coordinates: []
+  };
+
+  var points = pointStore(Object.keys(geoJson).map(function(k) { return geoJson[k]; }));
+
+  return topo;
 };
 
 exports.toGeo = function(topoJson) {
