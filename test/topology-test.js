11617295c2e9e1521bfb10fced90c72ee995816e
Mike Bostock <mbostock@gmail.com>
2012-12-03 11:34:59 -0800

    Allow multiple options to topology creation.

--- test/topology-test.js
+++ test/topology-test.js
@@ -16,7 +16,7 @@ suite.addBatch({
       assert.deepEqual(topology([
         {type: "LineString", coordinates: [[0, 0], [0, 1], [1, 1], [0, 0]]},
         {type: "LineString", coordinates: [[0, 0], [0, 1], [1, 1], [1, 2]]}
-      ], 3), {
+      ], {quantization: 3}), {
         type: "Topology",
         arcs: [[[0, 0], [0, 1], [2, 0], [-2, -1]],
                [[0, 0], [0, 1], [2, 0]],
@@ -30,7 +30,7 @@ suite.addBatch({
         {type: "Polygon", coordinates: [[[0, 0], [0, 2], [1, 2], [1, 1], [2, 1], [2, 0], [0, 0]]]},
         {type: "Polygon", coordinates: [[[3, 3], [3, 1], [2, 1], [2, 2], [1, 2], [1, 3], [3, 3]]]},
         {type: "Polygon", coordinates: [[[1, 1], [1, 2], [2, 2], [2, 1], [1, 1]]]},
-      ], 4), {
+      ], {quantization: 4}), {
         type: "Topology",
         arcs: [[[1, 2], [0, -1], [1, 0]],
                [[2, 1], [0, -1], [-2, 0], [0, 2], [1, 0]],
