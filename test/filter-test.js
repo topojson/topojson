3102ecc6f91f7a7c890b42f277a0c25c0133f4e6
Mike Bostock <mbostock@gmail.com>
2013-03-18 21:40:24 -0700

    More fixes related to coordinate systems.
    Fix up some dead code, and also employ safer rounding near spherical limits.

--- test/filter-test.js
+++ test/filter-test.js
@@ -14,23 +14,23 @@ suite.addBatch({
         feature: {type: "Feature", geometry: null},
         geometry: null
       });
-      filter(topology);
+      filter(topology, {"coordinate-system": "spherical"});
       assert.deepEqual(topology.objects.feature, {});
       assert.deepEqual(topology.objects.geometry, {});
     },
     "empty geometry objects are converted to null": function(filter) {
       var topology = topojson.topology({line: {type: "Polygon", coordinates: [[[0, 0], [1, 1], [1, 1], [0, 0]]]}});
-      filter(topology);
+      filter(topology, {"coordinate-system": "spherical"});
       assert.deepEqual(topology.objects.line, {type: null});
     },
     "small geometry objects are converted to null": function(filter) {
       var topology = topojson.topology({polygon: {type: "Polygon", coordinates: [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]]}});
-      filter(topology, {"minimum-area": .5});
+      filter(topology, {"coordinate-system": "spherical", "minimum-area": .5});
       assert.deepEqual(topology.objects.polygon, {type: null});
     },
     "big geometry objects are preserved": function(filter) {
       var topology = topojson.topology({polygon: {type: "Polygon", coordinates: [[[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]]]}});
-      filter(topology);
+      filter(topology, {"coordinate-system": "spherical"});
       assert.deepEqual(topology.objects.polygon, {type: "Polygon", arcs: [[0]]});
     }
   }
--- test/spherical-ringArea-test.js
+++ test/spherical-ringArea-test.js
@@ -1,13 +1,13 @@
 var vows = require("vows"),
     assert = require("assert"),
-    area = require("../lib/topojson/area");
+    spherical = require("../lib/topojson/spherical");
 
-var suite = vows.describe("topojson.area");
+var suite = vows.describe("topojson.spherical.ringArea");
 
 suite.addBatch({
-  "area": {
+  "ringArea": {
     topic: function() {
-      return area;
+      return spherical.ringArea;
     },
     "small clockwise area": function(area) {
       assert.inDelta(area([[0, -.5], [0, .5], [1, .5], [1, -.5], [0, -.5]]), 0.0003046212, 1e-10);
