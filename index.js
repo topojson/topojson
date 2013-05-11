084a34cfc2510a0d582383d7c02fb6c3512ec8fa
Mike Bostock <mbostock@gmail.com>
2013-05-11 14:49:56 -0700

    Add presimplify and resimplify.
    To facilitate interactive simplification, you can now call presimplify first,
    which computes the effective area of each point. Then, you can call resimplify
    to derive a filtered topology from the presimplified topology. This returns a
    new object, and thus can be called repeatedly for fast interactive filtering.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var fs = require("fs");
 
 var topojson = module.exports = new Function("topojson", "return " + fs.readFileSync(__dirname + "/topojson.js", "utf8"))();
 topojson.topology = require("./lib/topojson/topology");
+topojson.presimplify = require("./lib/topojson/presimplify");
+topojson.resimplify = require("./lib/topojson/resimplify");
 topojson.simplify = require("./lib/topojson/simplify");
 topojson.clockwise = require("./lib/topojson/clockwise");
 topojson.filter = require("./lib/topojson/filter");
