6dd23172f0512a2d72b2aad14cb7f2bcb96e38cd
Mike Bostock <mbostock@gmail.com>
2013-08-26 16:21:37 -0700

    Move non-topology functions.

--- test/transform-properties-test.js
+++ test/transform-properties-test.js
@@ -1,6 +1,6 @@
 var vows = require("vows"),
     assert = require("assert"),
-    transformProperties = require("../../lib/topojson/topology/transform-properties");
+    transformProperties = require("../lib/topojson/transform-properties");
 
 var suite = vows.describe("transform-properties");
 
