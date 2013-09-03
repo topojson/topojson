e77cb6ecefba25dc2053cc33680937d32e65562b
Mike Bostock <mbostock@gmail.com>
2013-09-03 09:18:26 -0700

    More tests for simplification.
    Adapted from @adamhooper’s pull request. Fixes #89.

---
+++ test/simplify-test.js
@@ -0,0 +1,129 @@
+var vows = require("vows"),
+    assert = require("./assert"),
+    simplify = require("../lib/topojson/simplify");
+
+var suite = vows.describe("simplify");
+
+suite.addBatch({
+  "simplify": {
+    "removes points with area below minimum area threshold": function() {
+      var topology = simplify({
+        transform: {scale: [1, 1], translate: [0, 0]},
+        objects: {},
+        arcs: [
+          [[0, 0], [10, 10], [0, 1], [1, -1], [-11, 0], [0, -10]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian",
+      });
+      assert.deepEqual(
+        topology.arcs,
+        [[[0, 0], [10, 10], [-10, 0], [0, -10]]]
+      );
+    },
+    "preserves points with area greater or equal to minimum area threshold": function() {
+      var topology = simplify({
+        transform: {scale: [1, 1], translate: [0, 0]},
+        objects: {},
+        arcs: [
+          [[0, 0], [10, 10], [0, 1], [1, -1], [-11, 0], [0, -10]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian",
+      });
+      assert.deepEqual(
+        topology.arcs,
+        [[[0, 0], [10, 10], [-10, 0], [0, -10]]]
+      );
+    },
+    "assumes delta-encoded arcs for quantized topologies": function() {
+      var topology = simplify({
+        transform: {scale: [1, 1], translate: [0, 0]},
+        objects: {},
+        arcs: [
+          [[0, 0], [10, 10], [0, 1], [1, -1], [-11, 0], [0, -10]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian",
+      });
+      assert.deepEqual(
+        topology.arcs,
+        [[[0, 0], [10, 10], [-10, 0], [0, -10]]]
+      );
+    },
+    "assumes non-delta-encoded arcs for non-quantized topologies": function() {
+      var topology = simplify({
+        objects: {},
+        arcs: [
+          [[0, 0], [10, 10], [10, 11], [11, 10], [0, 10], [0, 0]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian",
+      });
+      assert.deepEqual(
+        topology.arcs,
+        [[[0, 0], [10, 10], [0, 10], [0, 0]]]
+      );
+    },
+    "preserves degenerate arcs in non-quantized topologies": function() {
+      var topology = simplify({
+        objects: {},
+        arcs: [
+          [[50, 50], [50, 50]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian"
+      });
+      assert.deepEqual(topology.arcs, [
+        [[50, 50], [50, 50]]
+      ]);
+    },
+    "preserves degenerate arcs in quantized topologies": function() {
+      var topology = simplify({
+        transform: {scale: [1, 1], translate: [0, 0]},
+        objects: {},
+        arcs: [
+          [[50, 50], [0, 0]]
+        ]
+      }, {
+        "minimum-area": 2,
+        "coordinate-system": "cartesian"
+      });
+      assert.deepEqual(topology.arcs, [
+        [[50, 50], [0, 0]]
+      ]);
+    },
+    "does not remove empty arcs": function() {
+      var topology = simplify({
+        transform: {scale: [1, 1], translate: [0, 0]},
+        objects: {
+          foo: {
+            type: "LineString",
+            arcs: [0, 1, 2]
+          }
+        },
+        arcs: [
+          [[0, 0], [10, 0]],
+          [[10, 0], [1, 1], [-1, -1]],
+          [[10, 0], [0, 10]]
+        ]
+      }, {
+        "minimum-area": 1,
+        "coordinate-system": "cartesian"
+      });
+      assert.deepEqual(topology.arcs, [
+        [[0, 0], [10, 0]],
+        [[10, 0], [0, 0]],
+        [[10, 0], [0, 10]]
+      ]);
+      assert.deepEqual(topology.objects.foo.arcs, [0, 1, 2]);
+    }
+  }
+});
+
+suite.export(module);
