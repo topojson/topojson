4458bc62af432387f1566de2dc8b178aaf9c059c
Jason Davies <jason@jasondavies.com>
2013-11-24 11:53:20 +0000

    Add clockwise tests for cartesian coordinates.

--- test/clockwise-test.js
+++ test/clockwise-test.js
@@ -6,39 +6,78 @@ var suite = vows.describe("clockwise");
 
 suite.addBatch({
   "clockwise": {
-    "geometry": {
-      "ensures the ring with the largest absolute area is the exterior": function() {
-        var o = {
-          type: "Polygon",
-          coordinates: [
-            [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]],
-            [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]]
-          ]
-        };
-        clockwise(o, {"coordinate-system": "spherical"});
-        assert.deepEqual(o.coordinates, [
-          [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]],
-          [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]]
-        ]);
+    "spherical": {
+      "geometry": {
+        "ensures the ring with the largest absolute area is the exterior": function() {
+          var o = {
+            type: "Polygon",
+            coordinates: [
+              [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]],
+              [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]]
+            ]
+          };
+          clockwise(o, {"coordinate-system": "spherical"});
+          assert.deepEqual(o.coordinates, [
+            [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]],
+            [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]]
+          ]);
+        }
+      },
+      "topology": {
+        "ensures the ring with the largest absolute area is the exterior": function() {
+          var o = {
+            type: "Topology",
+            objects: {
+              polygon: {
+                type: "Polygon",
+                arcs: [[0], [1]]
+              }
+            },
+            arcs: [
+              [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]],
+              [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]]
+            ]
+          };
+          clockwise(o, {"coordinate-system": "spherical"});
+          assert.deepEqual(o.objects.polygon.arcs, [[1], [0]]);
+        }
       }
     },
-    "topology": {
-      "ensures the ring with the largest absolute area is the exterior": function() {
-        var o = {
-          type: "Topology",
-          objects: {
-            polygon: {
-              type: "Polygon",
-              arcs: [[0], [1]]
-            }
-          },
-          arcs: [
-            [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]],
-            [[0, 0], [0, 3], [3, 3], [3, 0], [0, 0]]
-          ]
-        };
-        clockwise(o, {"coordinate-system": "spherical"});
-        assert.deepEqual(o.objects.polygon.arcs, [[1], [0]]);
+    "cartesian": {
+      "geometry": {
+        "ensures the ring with the largest absolute area is the exterior": function() {
+          var o = {
+            type: "Polygon",
+            coordinates: [
+              [[1, 1], [1, 2], [2, 2], [2, 1], [1, 1]],
+              [[0, 0], [3, 0], [3, 3], [0, 3], [0, 0]]
+            ]
+          };
+          clockwise(o, {"coordinate-system": "cartesian"});
+          assert.deepEqual(o.coordinates, [
+            [[0, 0], [3, 0], [3, 3], [0, 3], [0, 0]],
+            [[1, 1], [1, 2], [2, 2], [2, 1], [1, 1]]
+          ]);
+        }
+      },
+      "topology": {
+        "ensures the ring with the largest absolute area is the exterior": function() {
+          var o = {
+            type: "Topology",
+            objects: {
+              polygon: {
+                type: "Polygon",
+                arcs: [[0], [1]]
+              }
+            },
+            arcs: [
+              [[1, 1], [1, 2], [2, 2], [2, 1], [1, 1]],
+              [[0, 0], [3, 0], [3, 3], [0, 3], [0, 0]]
+            ]
+          };
+          clockwise(o, {"coordinate-system": "cartesian"});
+          assert.deepEqual(o.objects.polygon.arcs, [[1], [0]]);
+        }
       }
     }
   }
