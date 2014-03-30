b8b3fb77e7948c3117428b06cb8cc7fb37ea135e
Mike Bostock <mbostock@gmail.com>
2014-03-30 14:28:34 -0700

    Empty arcs must have two points.

--- topojson.js
+++ topojson.js
@@ -16,10 +16,9 @@
         emptyIndex = -1;
 
     // Stitch empty arcs first, since they may be subsumed by other arcs.
-    // TODO Empty arcs should always have length 1?
     arcs.forEach(function(i, j) {
       var arc = topology.arcs[i < 0 ? ~i : i], t;
-      if (!arc[1] || !arc[2] && !arc[1][0] && !arc[1][1]) {
+      if (arc.length < 3 && !arc[1][0] && !arc[1][1]) {
         t = arcs[++emptyIndex], arcs[emptyIndex] = i, arcs[j] = t;
       }
     });
