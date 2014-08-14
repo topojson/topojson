58e49fe1535ff60d5c83c0b3cf58b488d7e94170
Jason Davies <jason@jasondavies.com>
2014-08-14 21:30:21 +0100

    Fix --simplify-proportion calculation.
    Previously, the minimum area required to retain a given proportion of
    points was incorrectly calculated when taking into account the ignored
    endpoints.

--- topojson.js
+++ topojson.js
@@ -1,6 +1,6 @@
 !function() {
   var topojson = {
-    version: "1.6.16",
+    version: "1.6.17",
     mesh: function(topology) { return object(topology, meshArcs.apply(this, arguments)); },
     meshArcs: meshArcs,
     merge: function(topology) { return object(topology, mergeArcs.apply(this, arguments)); },
