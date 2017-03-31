8c40a061406b0d1ec6a2171e30a27cb4316815d8
Mike Bostock <mbostock@gmail.com>
2017-03-30 22:51:39 -0700

    Use export *.

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-export {
-  topology
-} from "topojson-server";
-
-export {
-  filter,
-  filterAttached,
-  filterWeight,
-  planarRingArea,
-  planarTriangleArea,
-  presimplify,
-  quantile,
-  simplify,
-  sphericalRingArea,
-  sphericalTriangleArea
-} from "topojson-simplify";
-
-export {
-  bbox,
-  feature,
-  merge,
-  mergeArcs,
-  mesh,
-  meshArcs,
-  neighbors,
-  quantize,
-  transform,
-  untransform
-} from "topojson-client";
+export * from "topojson-client";
+export * from "topojson-server";
+export * from "topojson-simplify";
